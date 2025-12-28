21db7b6e03329db695187fc32abe9505a9750530
alac <alac@users.noreply.github.com>
2025-12-27 22:51:24 -0800

    Horizontal line separating turn switcher

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-34f40a71'; // Change value to force update
+const cacheName = 'kobold-e3ec5e76'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
