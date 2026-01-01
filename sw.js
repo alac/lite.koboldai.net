5279a89d87e2735f7f0338665a8592721126fe04
alac <alac@users.noreply.github.com>
2025-12-31 19:43:09 -0800

    Correct last_context_overlap_tokens

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-30820dc7'; // Change value to force update
+const cacheName = 'kobold-8affcd4a'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
