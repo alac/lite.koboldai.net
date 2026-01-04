d91f7697ac7c42b8c1f3de77965a9037901863aa
alac <alac@users.noreply.github.com>
2026-01-04 07:58:15 -0800

    Add a menu for "autogenerate menu" prompt presets

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-ef1c95e4'; // Change value to force update
+const cacheName = 'kobold-aa112206'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
