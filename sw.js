acce703ee9f872b0e2502533fa7c9bbecfb57846
alac <alac@users.noreply.github.com>
2026-01-06 15:45:52 -0800

    avatar ui no longer floats

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-ed839dab'; // Change value to force update
+const cacheName = 'kobold-c92f5caa'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
