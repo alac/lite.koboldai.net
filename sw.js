ec63a7f6e2fc57f37986596e6538e574e772abb5
alac <alac@users.noreply.github.com>
2026-01-06 16:09:08 -0800

    Create parallel draft data even if there's only one model

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-c92f5caa'; // Change value to force update
+const cacheName = 'kobold-f3e8c735'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
