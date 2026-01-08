45603d25c3d79c1439fce4eebdf25dbe36edf3f4
alac <alac@users.noreply.github.com>
2026-01-08 02:27:13 -0800

    Fix loading error

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-fc004e48'; // Change value to force update
+const cacheName = 'kobold-4b9d3cb2'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
