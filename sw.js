e367e5d4bd6b49547f623657ebb8527056decbb7
alac <alac@users.noreply.github.com>
2026-01-08 05:22:28 -0800

    Gracefully handle errors in generation

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-9d1e7e61'; // Change value to force update
+const cacheName = 'kobold-877b7e36'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
