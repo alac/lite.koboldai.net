784c4fab1275cd988d22792de589aa6d808e6ee2
alac <alac@users.noreply.github.com>
2026-01-08 18:08:58 -0800

    Update index.html

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-877b7e36'; // Change value to force update
+const cacheName = 'kobold-5e97b778'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
