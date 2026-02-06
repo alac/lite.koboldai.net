26078466495c658e8b7c97023790a00d5d666ebe
alac <alac@users.noreply.github.com>
2026-02-06 03:14:11 -0800

    Empty parallel responses still have formatting

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-c7ea0fa9'; // Change value to force update
+const cacheName = 'kobold-e3a18147'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
