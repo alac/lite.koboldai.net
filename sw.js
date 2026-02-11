a9226dbbb82c54eb97c146d4e14e63fcd7a72626
alac <alac@users.noreply.github.com>
2026-02-10 16:32:51 -0800

    Apply fixes menu: special case strict removals to not count as already applied

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-e3a18147'; // Change value to force update
+const cacheName = 'kobold-af3f0a99'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
