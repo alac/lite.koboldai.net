a10aea79470f778bac485998a62ba356eb65734a
alac <alac@users.noreply.github.com>
2026-02-10 20:54:23 -0800

    If a draft version is empty, put empty text there

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-af3f0a99'; // Change value to force update
+const cacheName = 'kobold-fa276290'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
