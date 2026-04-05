a171089cd739a00d81e9fdae6c17579dd5c1f230
alac <alac@users.noreply.github.com>
2026-04-05 14:53:50 -0700

    draft switcher ux: row of buttons -> dropdown with prev/next

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-52d233f5'; // Change value to force update
+const cacheName = 'kobold-e36126d8'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
