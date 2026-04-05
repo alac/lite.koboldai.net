6c2eee1f39c7421dd5f82000a4c2200e44c404ad
alac <alac@users.noreply.github.com>
2026-04-05 16:54:26 -0700

    put the switcher under the input field

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-e36126d8'; // Change value to force update
+const cacheName = 'kobold-c1d844cf'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
