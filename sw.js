93e532bea512be2d53e468c26fada70bd68cba1b
alac <alac@users.noreply.github.com>
2026-04-05 06:09:44 -0700

    Try (?) to make draft switching work in Story Mode

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-e1fe7397'; // Change value to force update
+const cacheName = 'kobold-c4eac2b0'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
