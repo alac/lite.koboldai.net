4d83b692b302ca56f2bb22ac1b7837e5dd915ef3
alac <alac@users.noreply.github.com>
2026-04-05 05:32:29 -0700

    Handle reasoning and content in the same delta

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-1226e98c'; // Change value to force update
+const cacheName = 'kobold-e1fe7397'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
