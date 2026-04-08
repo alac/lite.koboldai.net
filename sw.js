f4eb9514021e99c770e0e768098dd788ef9b47c5
alac <alac@users.noreply.github.com>
2026-04-08 00:34:25 -0700

    Parallel draft switcher live updates request progress

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-bf87ba76'; // Change value to force update
+const cacheName = 'kobold-fdcbfe1e'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
