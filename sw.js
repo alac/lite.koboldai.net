e81fbe8debe66a695f7c1176e4ee1a5961923922
alac <alac@users.noreply.github.com>
2026-04-08 00:25:12 -0700

    Add a jump button if the chat is long

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'kobold-c1d844cf'; // Change value to force update
+const cacheName = 'kobold-bf87ba76'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
